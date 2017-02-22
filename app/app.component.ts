5ef764bdb765c93c21bf457562791294868a2242
Harry Ferrier <f3rr13r@hotmail.com>
2017-02-22 16:48:10 +0000

    Finalized app as per tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+import { Task } from './model/task';
+
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>Skeleton Project</h1>'
+    templateUrl: 'app.component.html',
+    styleUrls: [ 'app.component.css' ]
 })
 
-export class AppComponent { }
+export class AppComponent {
+    private tasks: Task[] = [];
+
+    private currentTask = new Task(null, false);
+
+    private addTask() {
+        // Create local instance of task containing current task content and isCompleted values.
+        let task = new Task(this.currentTask.content, this.currentTask.isCompleted);
+
+        // Push to the tasks array.
+        this.tasks.push(task);
+
+        // Clear the input field
+        this.currentTask.content = null;
+    }
+ }
---
+++ app/model/task.js
@@ -0,0 +1,10 @@
+"use strict";
+var Task = (function () {
+    function Task(content, isCompleted) {
+        this.content = content;
+        this.isCompleted = isCompleted;
+    }
+    return Task;
+}());
+exports.Task = Task;
+//# sourceMappingURL=task.js.map
